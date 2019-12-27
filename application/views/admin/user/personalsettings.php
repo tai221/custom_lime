ac097999fa39441607c7bf036176680ea1bbac81
TaiNV <tainv@kaopiz.com>
2019-12-27 14:26:25 +0700

    remove setting account

--- application/views/admin/user/personalsettings.php
+++ application/views/admin/user/personalsettings.php
@@ -10,12 +10,8 @@
         <div class="col-xs-12">
             <ul class="nav nav-tabs" role="tablist">
                 <li role="presentation" class="active"><a href="#your-profile" role="tab" data-toggle="tab"><?php eT("My profile"); ?></a></li>
-                <li role="presentation"><a href="#your-personal-settings" role="tab" data-toggle="tab"><?php eT("My personal settings"); ?></a></li>
-                <li role="presentation" ><a href="#your-personal-menues" role="tab" data-toggle="tab"><?php eT("My personal menus"); ?></a></li>
-                <li role="presentation" ><a href="#your-personal-menueentries" role="tab" data-toggle="tab"><?php eT("My personal menu entries"); ?></a></li>
             </ul>
             <div class="tab-content">
-
                 <!-- TAB: My profile settings -->
                 <div role="tabpanel" class="tab-pane fade in active" id="your-profile">
                     <div class="pagetitle h3"><?php eT("My profile"); ?></div>
@@ -63,7 +59,7 @@
                                     <i class="fa fa-lock"></i>
                                     <?=gT("Change password")?>
                                 </button>
-                                
+
                                 <br/>
                             </div>
                         </div>
@@ -77,7 +73,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-6">                            
+                            <div class="col-md-6">
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
@@ -97,120 +93,6 @@
                             </div>
                         </div>
                     </div>
-                </div>
-
-                <!-- TAB: My personal settings -->
-                <div role="tabpanel" class="tab-pane fade" id="your-personal-settings">
-                    <div class="pagetitle h3"><?php eT("My personal settings"); ?></div>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6">
-                                <!-- Interface language -->
-                                <div class="form-group">
-                                    <?php echo CHtml::label(gT("Interface language:"), 'lang', array('class'=>" control-label")); ?>
-                                    <div class="selector_contain_select2">
-                                        <?php
-                                        $this->widget('yiiwheels.widgets.select2.WhSelect2', array(
-                                            'asDropDownList' => true,
-                                            'name' => 'lang',
-                                            'data' => $aLanguageData,
-                                            'pluginOptions' => array(
-                                                'buttonWidth' => '100%',
-                                                'htmlOptions' => array(
-                                                    'id' => 'lang',
-                                                    'style'=> "widht:100%;"
-                                                )
-                                            ),
-                                            'value' => $sSavedLanguage,
-                                            'htmlOptions' => array(
-                                                'class'=> "form-control",
-                                                'style'=> "widht:100%;",
-                                                'data-width' => '100%'
-                                            )
-                                        ));
-
-                                            ?>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-md-6">
-                                <!-- HTML editor mode -->
-                                <div class="form-group">
-                                    <?php echo CHtml::label(gT("HTML editor mode:"), 'htmleditormode', array('class'=>" control-label")); ?>
-                                    <div class="">
-                                        <?php
-                                            echo CHtml::dropDownList('htmleditormode', Yii::app()->session['htmleditormode'], array(
-                                                'default' => gT("Default",'unescaped'),
-                                                'inline' => gT("Inline HTML editor",'unescaped'),
-                                                'popup' => gT("Popup HTML editor",'unescaped'),
-                                                'none' => gT("No HTML editor",'unescaped')
-                                            ), array('class'=>"form-control"));
-                                        ?>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12 col-md-6">
-                                <!-- Question type selector -->
-                                <div class="form-group">
-                                    <?php echo CHtml::label(gT("Question type selector:"), 'questionselectormode', array('class'=>" control-label")); ?>
-                                    <div class="">
-                                        <?php
-                                        echo CHtml::dropDownList('questionselectormode', Yii::app()->session['questionselectormode'], array(
-                                            'default' => gT("Default",'unescaped'),
-                                            'full' => gT("Full selector",'unescaped'),
-                                            'none' => gT("Simple selector",'unescaped')
-                                        ), array('class'=>"form-control"));
-                                        ?>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-md-6">
-                                <!-- Template editor mode -->
-                                <div class="form-group">
-                                    <?php echo CHtml::label(gT("Template editor mode:"), 'templateeditormode', array('class'=>" control-label")); ?>
-                                    <div class="">
-                                        <?php
-                                        echo CHtml::dropDownList('templateeditormode', Yii::app()->session['templateeditormode'], array(
-                                            'default' => gT("Default"),
-                                            'full' => gT("Full template editor"),
-                                            'none' => gT("Simple template editor")
-                                        ), array('class'=>"form-control"));
-                                        ?>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-md-6">
-                                <!-- Date format -->
-                                <div class="form-group">
-                                    <?php echo CHtml::label( gT("Date format:"), 'dateformat', array('class'=>" control-label")); ?>
-                                    <div class="">
-                                        <select name='dateformat' id='dateformat' class="form-control">
-                                            <?php
-                                            foreach (getDateFormatData(0,Yii::app()->session['adminlang']) as $index => $dateformatdata)
-                                            {
-                                                echo "<option value='{$index}'";
-                                                if ($index == Yii::app()->session['dateformat'])
-                                                {
-                                                    echo " selected='selected'";
-                                                }
-
-                                                echo ">" . $dateformatdata['dateformat'] . '</option>';
-                                            }
-                                            ?>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div role="tabpanel" class="tab-pane fade" id="your-personal-menues">
-                    <?php $this->renderPartial('/admin/surveymenu/shortlist', $surveymenu_data); ?>
-                </div>
-                <div role="tabpanel" class="tab-pane fade" id="your-personal-menueentries">
-                    <?php $this->renderPartial('/admin/surveymenu_entries/shortlist', $surveymenuentry_data); ?>
                 </div>
             </div>
         </div>
